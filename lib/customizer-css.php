21d1231467cc5eb4bccd7bb6a29c2393f79838a2
steelbridge-io <chris@steelbridge.io>
2020-08-29 12:35:15 -0700

    Updated Nav's

--- lib/customizer-css.php
+++ lib/customizer-css.php
@@ -2,10 +2,14 @@
 
 function load_wst_customizer_css() {
 	$css_wst = '';
-	$subnav_bgcolor = get_theme_mod ( 'subnav_bgcolor');
+	$subnav_bgcolor = get_theme_mod( 'subnav_bgcolor');
+	$subnav_fcolor	= get_theme_mod('subnav_fcolor');
 	$css_wst .= '
 		#sub-nav-fp {
 			background-color: ' . $subnav_bgcolor . ';
+		}
+		.subnav li a {
+			color: '. $subnav_fcolor .';
 		}
 	';
 	return $css_wst;
--- lib/customizer-sections/sub-nav.php
+++ lib/customizer-sections/sub-nav.php
@@ -5,6 +5,25 @@ $wp_customize->add_section( 'subNav', array(
 	'title' => __( 'Sub Navigation', 'bootstrap-for-genesis' ),
 	'priority' => 10
 ));
+// Sub-Nav Font Color
+$wp_customize->add_setting ( 'subnav_fcolor', array(
+	'default'						=> '#000',
+	'type'								=> 'theme_mod',
+	'sanitize_callback'	=> 'sanitize_hex_color',
+
+));
+// Sub-Nav Background Font Picker
+$wp_customize -> add_control (
+	new WP_Customize_Color_Control (
+		$wp_customize,
+		'subnav_fgcolor', array(
+			'label'			=> __('Sub Nav Font Color', 'bootstrap-for-genesis' ),
+			'priority'		=>  10,
+			'section'		=> 'subNav',
+			'settings'	=> 'subnav_fcolor'
+		)
+	)
+);
 // Sub-Nav Background Color Picker
 $wp_customize->add_setting ( 'subnav_bgcolor', array(
 	'default'						=> '#ccc',
@@ -12,7 +31,6 @@ $wp_customize->add_setting ( 'subnav_bgcolor', array(
 	'sanitize_callback'	=> 'sanitize_hex_color',
 
 ));
-
 // Sub-Nav Background Color Picker
 $wp_customize -> add_control (
 	new WP_Customize_Color_Control (
