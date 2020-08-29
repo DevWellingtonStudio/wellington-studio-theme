4bc593939319619400a66d5129bf718db299af10
steelbridge-io <chris@steelbridge.io>
2020-08-29 02:21:39 -0700

    Added Portrait Slider

--- inc/portrait-slider.php
+++ inc/portrait-slider.php
@@ -20,7 +20,7 @@
 				<?php $p_slider_text1 = get_theme_mod('p-slider-text1'); ?>
 				<p class="card-text"><?php if($p_slider_text1 !== ''){echo $p_slider_text1;} ?></p>
 				<?php $port1_btn_url = get_theme_mod('port1_btn_url'); ?>
-				<?php if($port1_btn_url !== ''){echo '<a href="' . $port1_btn_url . '" title="Read More"><button type="button" class="btn btn-dark"><h5>Find Out More...</h5></button></a>';} ?>
+				<?php if($port1_btn_url !== ''){echo '<a href="' . $port1_btn_url . '" title="Read More"><button type="button" class="zindex-btn btn btn-dark"><h5>Find Out More...</h5></button></a>';} ?>
 			 </div>
 			</div>
 			<div class="col-md-6 portrait-image">
@@ -44,7 +44,7 @@
 			   <?php $p_slider_text2 = get_theme_mod('p-slider-text2'); ?>
 				<p class="card-text"><?php if($p_slider_text2 !== ""){echo $p_slider_text2;} ?></p>
 				<?php $port2_btn_url = get_theme_mod('port2_btn_url'); ?>
-				<?php if($port2_btn_url !== ''){echo '<a href="' . $port2_btn_url . '" title="Read More"><button type="button" class="btn btn-dark"><h5>Read More Here...</h5></button></a>';} ?>
+				<?php if($port2_btn_url !== ''){echo '<a href="' . $port2_btn_url . '" title="Read More"><button type="button" class="zindex-btn btn btn-dark"><h5>Read More Here...</h5></button></a>';} ?>
 			 </div>
 			</div>
 		 </div>
@@ -55,13 +55,17 @@
 		 <div class="row no-gutters">
 			<div class="col-md-6 card-body-wrapper">
 			 <div class="card-body">
-				<h1 class="card-title">Section Title</h1>
-				<p class="card-text">This area will have more style added. Font size will be updated, options to change background color. The image to the right should always be an image that can be rendered in portrait orientation for best results. Consider when viewing this section on a mobile device, the text "could" render over the image. Contast can be optimized via a filter on the image when viewed on devices smaller than a tablet.</p>
-				<button type="button" class="btn btn-dark"><h5>This Button Links To Content</h5></button>
+			   <?php $p_slider_title3 = get_theme_mod('p-slider-title3'); ?>
+				<h1 class="card-title"><?php if($p_slider_title3 !== ''){echo $p_slider_title3;} ?></h1>
+			   <?php $p_slider_text3 = get_theme_mod('p-slider-text3'); ?>
+				<p class="card-text"><?php if($p_slider_text3 !== ''){echo $p_slider_text3;} ?></p>
+				<?php $port3_btn_url = get_theme_mod('port3_btn_url'); ?>
+				<?php if($port3_btn_url !== ''){ echo '<a href="' . $port3_btn_url . '" title="Read More"><button type="button" class="zindex-btn btn btn-dark"><h5>Read More...</h5></button></a>';} ?>
 			 </div>
 			</div>
 			<div class="col-md-6 portrait-image">
-			 <img src="/wp-content/uploads/2020/08/pexels-luis-quintero-1408196-scaled.jpg" class="card-img" alt="...">
+			  <?php $port_slider3 = get_theme_mod('port_slider3'); ?>
+			 <img src="<?php if($port_slider3 !== ''){echo $port_slider3;} ?>" class="card-img" alt="...">
 			</div>
 		 </div>
 		</div>
