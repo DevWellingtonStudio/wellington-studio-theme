767edc7d7a480317e6e45a402f19786f12475d7a
steelbridge-io <chris@steelbridge.io>
2020-08-29 01:47:39 -0700

    Added Portrait Slider

--- inc/portrait-slider.php
+++ inc/portrait-slider.php
@@ -34,14 +34,17 @@
 		<div id="portrait-hero" class="card">
 		 <div class="row no-gutters">
 			<div class="col-md-6 portrait-image">
-			 <img src="/wp-content/uploads/2020/08/pexels-thirdman-5060987-scaled.jpg" class="card-img" alt="...">
+			  <?php $port_slider2 = get_theme_mod('port_slider2'); ?>
+			 <img src="<?php if($port_slider2 !== ''){echo $port_slider2;} ?>" class="card-img" alt="...">
 			</div>
 			<div class="col-md-6 card-body-wrapper">
 			 <div class="card-body">
 			   <?php $p_slider_title2 = get_theme_mod('p-slider-title2'); ?>
 				<h1 class="card-title"><?php if($p_slider_title2 !== ''){echo $p_slider_title2;} ?></h1>
-				<p class="card-text">This area will have more style added. Font size will be updated, options to change background color. The image to the right should always be an image that can be rendered in portrait orientation for best results. Consider when viewing this section on a mobile device, the text "could" render over the image. Contast can be optimized via a filter on the image when viewed on devices smaller than a tablet.</p>
-				<button type="button" class="btn btn-dark"><h5>This Button Links To Content</h5></button>
+			   <?php $p_slider_text2 = get_theme_mod('p-slider-text2'); ?>
+				<p class="card-text"><?php if($p_slider_text2 !== ""){echo $p_slider_text2;} ?></p>
+				<?php $port2_btn_url = get_theme_mod('port2_btn_url'); ?>
+				<?php if($port2_btn_url !== ''){echo '<a href="' . $port2_btn_url . '" title="Read More"><button type="button" class="btn btn-dark"><h5>Read More Here...</h5></button></a>';} ?>
 			 </div>
 			</div>
 		 </div>
