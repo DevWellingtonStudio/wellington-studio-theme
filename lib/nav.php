01efca193d656beb7c1e2f35589a071bbd723788
steelbridge-io <chris@steelbridge.io>
2020-09-23 10:43:09 -0700

    Updated footer widgets scss

--- lib/nav.php
+++ lib/nav.php
@@ -174,7 +174,7 @@ $menu_locations = get_nav_menu_locations();
 if($menu_locations && $menu_locations['subnav'] !=0) :
  ?>
  <nav id="sub-nav-fp" class="navbar navbar-expand-md navbar-light" role="navigation">
-	<div class="container">
+	<div class="container-fluid">
 	 <!-- Brand and toggle get grouped for better mobile display -->
 	 <button class="navbar-toggler subnav-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
 		<span class="navbar-toggler-icon"></span>
