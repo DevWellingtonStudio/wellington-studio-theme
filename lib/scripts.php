dea1f84cfa7690815f712ac4c32c612171ada286
steelbridge-io <chris@steelbridge.io>
2020-10-08 13:59:11 -0700

    Added scss updates to nav-bar-toggle-icon

--- lib/scripts.php
+++ lib/scripts.php
@@ -44,6 +44,9 @@ function bfg_theme_scripts() {
 		
 		wp_register_script( 'custom-js', BFG_THEME_JS . 'custom.js', array(), $version, true );
 		wp_enqueue_script('custom-js');
+		
+		wp_register_script( 'font-awesome', 'https://kit.fontawesome.com/76342ff491.js', array(), $version, true );
+		wp_enqueue_script('font-awesome');
 	}
 }
 
