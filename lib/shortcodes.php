ec81f0a1001bc7dd546e4c45028881582039ceba
steelbridge-io <chris@steelbridge.io>
2020-10-14 10:28:15 -0700

    Updated shortcode name

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -6,7 +6,7 @@
 
 /* Parallax Scroll */
 
-add_shortcode('prallax-scroll', 'parallax_scroll_shortcode');
+add_shortcode('parallax-scroll', 'parallax_scroll_shortcode');
 function parallax_scroll_shortcode() {
 	return '<div class="parallax-window" data-parallax="scroll" data-image-src="https://dev2.wellington-studio.com/wp-content/uploads/2020/10/BCZ-Map_D4-01-scaled.jpg"></div>';
 }
